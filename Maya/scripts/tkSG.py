c32f518cff99af118722ccd1b901cccd8a38a76e
Cyril Gibaud <cyril.gibaud@toonkit-studio.com>
2020-02-28 18:52:05 +0100

    Added a wrapper for Shotgun initialization to add certificates on the fly

--- Maya/scripts/tkSG.py
+++ Maya/scripts/tkSG.py
@@ -2,8 +2,18 @@ import sys
 import os
 
 sgPath = os.path.join(os.path.dirname(__file__),"pythonApi")
+certsPath = os.path.join(sgPath, "shotgun_api3", "lib", "httplib2", "python2", "cacerts.txt")
 
 if not sgPath in sys.path:
 	sys.path.append(sgPath)
 
-import shotgun_api3 as shotgun
+from shotgun_api3 import *
+from shotgun_api3 import Shotgun as ShotgunOrig
+
+#Implement certificates bugfix (https://developer.shotgunsoftware.com/c593f0aa/)
+def Shotgun(*args, **kwargs):
+	#Add ca_certs on the fly if not given
+	if not "ca_certs" in kwargs:
+		kwargs["ca_certs"] = certsPath
+
+	return ShotgunOrig(*args, **kwargs)
